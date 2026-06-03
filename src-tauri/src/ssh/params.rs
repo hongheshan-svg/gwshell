9fbe618f7846586f1e5e9035cd3b777fa28a9f28
zhengshan <zhengshan@gw-link.com>
2026-06-04 06:35:55 +0800

    feat(ssh): ConnectParams for russh module

--- src-tauri/src/ssh/params.rs
+++ src-tauri/src/ssh/params.rs
@@ -1,1 +1,22 @@
-// placeholder
+#[derive(Clone, Debug)]
+pub struct ConnectParams {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: Option<String>,
+    pub private_key_path: Option<String>,
+    pub auth_method: String,
+    pub totp_code: Option<String>,
+    pub jump_host: Option<String>,
+    pub jump_port: u16,
+    pub jump_username: Option<String>,
+    pub jump_password: Option<String>,
+    pub jump_private_key_path: Option<String>,
+    pub proxy_type: Option<String>,
+    pub proxy_host: Option<String>,
+    pub proxy_port: u16,
+    pub proxy_username: Option<String>,
+    pub proxy_password: Option<String>,
+    pub connection_timeout: u32,
+    pub idle_disconnect_minutes: u32,
+}
